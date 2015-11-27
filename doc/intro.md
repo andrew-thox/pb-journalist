eabfbccccc21d1c3fe7e30d9043c985c8d489720
Andrew Kenyon <andrewkenyon123@gmail.com>
2015-11-27 23:22:01 +0000

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to journalist
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/journalist/core_test.clj
@@ -0,0 +1,7 @@
+(ns journalist.core-test
+  (:require [clojure.test :refer :all]
+            [journalist.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
