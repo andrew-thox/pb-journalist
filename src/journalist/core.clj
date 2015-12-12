d224e7d4e952db162854bb37f1310b66044ab7be
Andrew Kenyon <andrewkenyon123@gmail.com>
2015-12-12 15:05:25 +0000

    trying to debug rabbitmq tutum connection (added log statement)

--- src/journalist/core.clj
+++ src/journalist/core.clj
@@ -1,7 +1,10 @@
 (ns journalist.core
   (:gen-class)
   (:use  ring.adapter.jetty)
-  (:require [journalist.web.routes :as web]))
+  (:require [journalist.web.routes :as web]
+            [journalist.logging.log :as log]
+            [environ.core :refer [env]]))
 
 (defn -main [& args]
+  (log/info (env :amqp-url))
   (run-jetty #'web/app {:port 8080}))
