5294a452968c81d17038195b2e1aa99a78a5b4cb
Andrew Kenyon <andrewkenyon123@gmail.com>
2015-12-27 14:27:33 +0000

    can now parse Independent RSS articles

---
+++ src/journalist/the_independent/rss_reader.clj
@@ -0,0 +1,23 @@
+(ns journalist.the-independent.rss-reader
+  (:use clojure.string)
+  (:require [net.cgrand.enlive-html :as html]
+            [clj-time.format :as f]
+            [clj-time.core :as t]))
+
+(def rss-feeds {:news "http://www.independent.co.uk/news/rss"})
+(def ISO8601-X-formatter (f/formatter (t/default-time-zone) "YYYY-MM-dd HH:mm:ss" "YYYY-MM-dd'T'HH:mm:ssZ"))
+
+(defn process-article [article]
+  (let [article-map {:author (join (map html/text (html/select article [:author])))
+                     :title (join (map html/text (html/select article [:title])))
+                     :link (join (map html/text (html/select article [:link])))
+                     :publication "The Independent"
+                     :publish_date (f/unparse ISO8601-X-formatter
+                      (f/parse ISO8601-X-formatter (join (map html/text (html/select article [:dc:date])))))
+                     :acquistion_date (f/unparse ISO8601-X-formatter (t/now))}]
+    article-map))
+
+(defn process-rss-feed [feed]
+  (let [feed ((keyword feed) rss-feeds)
+        articles (html/select (html/xml-resource (java.net.URL. feed)) [:item])]
+    (process-article (first articles))))
