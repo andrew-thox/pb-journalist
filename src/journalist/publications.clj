b7ce9e5ae9be60be36caab958adfbc92a4065fc5
Andrew Kenyon <andrewkenyon123@gmail.com>
2016-01-06 00:01:23 +0000

    added the independent to publications

--- src/journalist/publications.clj
+++ src/journalist/publications.clj
@@ -3,4 +3,6 @@
  (def outlets '[
                {:slug "new-statesman"
                 :rss-feed "journalist.new_statesman.rss_reader/process_rss_feed"}
+               {:slug "the-independent"
+                :rss-feed "journalist.the_independent.rss_reader/process-rss-feed"}
                ])
