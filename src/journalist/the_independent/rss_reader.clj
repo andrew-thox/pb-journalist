b3b7364995279a8e52f3009a542874c4ca4bdeeb
Andrew Kenyon <andrewkenyon123@gmail.com>
2016-01-06 00:00:29 +0000

    changed namespace added support for multiple feeds per outlet

--- src/journalist/the_independent/rss_reader.clj
+++ src/journalist/the_independent/rss_reader.clj
@@ -1,10 +1,13 @@
-(ns journalist.the-independent.rss-reader
+(ns journalist.the_independent.rss_reader
   (:use clojure.string)
   (:require [net.cgrand.enlive-html :as html]
             [clj-time.format :as f]
-            [clj-time.core :as t]))
+            [clj-time.core :as t]
+            [journalist.logging.log :as log]))
 
-(def rss-feeds {:news "http://www.independent.co.uk/news/rss"})
+(def rss-feeds {:default "http://www.independent.co.uk/news/rss"
+                :news "http://www.independent.co.uk/news/rss"})
+
 (def ISO8601-X-formatter (f/formatter (t/default-time-zone) "YYYY-MM-dd HH:mm:ss" "YYYY-MM-dd'T'HH:mm:ssZ"))
 
 (defn process-article [article]
@@ -18,8 +21,9 @@
     article-map))
 
 ;TODO: This function can be shared across all outlets
-(defn process-rss-feed [feed]
+(defn process-rss-feed [ & [feed-key]]
   (log/info "parsing The Independent RSS feed")
-  (let [feed ((keyword feed) rss-feeds)
+  (let [feed-key (or feed-key :default)
+        feed ((keyword feed-key) rss-feeds)
         articles (html/select (html/xml-resource (java.net.URL. feed)) [:item])]
     (process-article (first articles))))
