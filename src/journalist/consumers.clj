ae4ddd3dc624965198ecbe528c855eb15cb4a1cc
Andrew Kenyon <andrewkenyon123@gmail.com>
2017-04-23 17:19:23 +0100

    now capable of processing tasks to get evening standard articles

--- src/journalist/consumers.clj
+++ src/journalist/consumers.clj
@@ -7,7 +7,9 @@
             [journalist.publish :as queue]
             [journalist.logging.log :as log]
             ;It would be cool if we could have a better way to do this
-            [journalist.new_statesman.rss_reader]))
+            [journalist.new_statesman.rss_reader]
+            [journalist.evening_standard.rss_reader]
+            ))
 
 (defn task-consumer
   [ch {:keys [content-type delivery-tag] :as meta} ^bytes payload]
--- src/journalist/publications.clj
+++ src/journalist/publications.clj
@@ -3,4 +3,6 @@
  (def outlets '[
                {:slug "new-statesman"
                 :rss-feed "journalist.new_statesman.rss_reader/process_rss_feed"}
+               {:slug "evening-standard"
+           		:rss-feed "journalist.evening_standard.rss_reader/process_rss_feed"}
                ])
